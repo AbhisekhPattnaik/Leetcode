dcf78165438a5811014f1597bb8173b3fae42069
Abhisekh Pattnaik <abhisekhpattnaik807@gmail.com>
2026-08-15 22:03:41 +0530

    Time: 7 ms (41.39%), Space: 13.9 MB (7.1%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        a=set(nums)
+        for i in range(len(nums)+1):
+            if i not in a:
+                return i
+        
+        
